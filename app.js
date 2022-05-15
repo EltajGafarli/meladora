72cd12ff40f7cf27ab806cd74dc26b286fe779c7
EltajGafarli <gafarlieltaj@gmail.com>
2022-05-15 18:33:00 +0400

    adding menu files

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ $("#searchIcon").click(function(e) {
 $("#cancel").click(function(e) {
     $(".searchArea").slideUp("slow");
 });
+
+$("#menu-button").click(function(e) {
+    $(".menu-for-mobile").show(1000);
+});
+
+$("#cancel-for-menu").click(function(e) {
+    $(".menu-for-mobile").hide(1000);
+})
