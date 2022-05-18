bdfc2f6b2dc174f9f74ee4943d1520479bd1fe7d
EltajGafarli <gafarlieltaj@gmail.com>
2022-05-19 02:13:04 +0400

    everything is good for now

---
+++ maladora-whoweare/app.js
@@ -0,0 +1,16 @@
+$("#searchIcon").click(function(e) {
+    $(".searchArea").slideDown("fast");
+
+});
+
+$("#cancel").click(function(e) {
+    $(".searchArea").slideUp("fast");
+});
+
+$("#menu-button").click(function(e) {
+    $(".menu-for-mobile").show(1000);
+});
+
+$("#cancel-for-menu").click(function(e) {
+    $(".menu-for-mobile").hide(1000);
+})
